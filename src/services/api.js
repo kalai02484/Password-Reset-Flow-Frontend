a8a6db85e4ed0b8f1ebfdd3b8807a9a0af18c7dc
kalai02484 <kalai02484@gmail.com>
2026-03-27 19:40:50 +0530

    changes done in api.js

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const api = axios.create({
   //baseURL: "http://localhost:5000/api",
   baseURL: "https://password-reset-flow-backend-ata2.onrender.com/api",
-}); ss
+});
 
 api.interceptors.request.use(
   (config) => {
